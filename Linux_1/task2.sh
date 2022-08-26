829e93e1ff7b65085dd5aaff7e42ba514d8091d2
D-on3 <104354779+D-on3@users.noreply.github.com>
2022-08-26 19:57:43 +0300

    init

---
+++ Linux_1/task2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Използвайки команди, разберете на какво са зададени в момента
+#username, hostname, shell и текущата директория.
+
+whoami
+hostname
+grep aod /etc/passwd
+pwd
+
+
+
+
+
+
